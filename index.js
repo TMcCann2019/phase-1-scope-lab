911bbe603a2174d37159c69ba055e332f51a9d8f
TimothyMcCann <timothysmccann@gmail.com>
2023-12-11 19:24:29 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName= 'bob'
+function upperCaseCustomerName(){
+    if (customerName){
+        customerName = customerName.toUpperCase()
+    }
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    console.log(bestCustomer)
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    console.log(bestCustomer)
+}
+
+const leastFavoriteCustomer = 'Tom'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'not tom'
+    console.log(leastFavoriteCustomer)
+}
